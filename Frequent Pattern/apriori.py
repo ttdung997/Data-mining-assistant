fee5c8449b6857f91a7cb30493a055c55bf3dd99
mrhieusd <mrhieusd@gmail.com>
2019-09-25 09:08:55 +0700

    apriori update

--- Frequent Pattern/apriori.py
+++ Frequent Pattern/apriori.py
@@ -12,3 +12,10 @@ transactions = [('eggs', 'bacon', 'soup'),
 itemsets, rules = apriori(transactions, min_support=0.5,  min_confidence=1)
 
 print(rules)  # [{eggs} -> {bacon}, {soup} -> {bacon}]
+
+# Them rang buoc cho luat
+# lhs: luat trai, rhs: luat phai
+
+rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 1, rules)
+for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
+	print(rule) # In luat va cac cac chi so danh gia
